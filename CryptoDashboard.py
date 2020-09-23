5620e93556b03ee4eacc185022ca8f1a955a755b
rfkinline <robert@lightdec.com>
2020-09-23 12:25:37 -0400

    updated documentation

--- CryptoDashboard.py
+++ CryptoDashboard.py
@@ -12,12 +12,18 @@ from json import loads
 
 gpio = pigpio.pi()
 
+investusd = 8.44
+investbtc = 0.77
 specialcoin1 = 'lend'
 specialcoin2 = 'ethereum'
 pricebtc = 0
 btc = 0
 summe = 0
 onlyonce = 0
+summemax = 0
+summemaxtime = 0
+btcmax = 0
+btcmaxtime = 0
 btcmaxtemp = 0
 sellcoinpercsav = 0
 sellcoin = ' '
@@ -25,57 +31,109 @@ sellcoinsav = ' '
 
 now = datetime.datetime.now()
 df = pd.read_csv('portfolio.csv', delimiter=';', names = ['Coin', 'Qty', 'Purchase'])
+tf = pd.read_csv('ConfigCryptoDashboard.csv', delimiter=';', names = ['Name', 'Value', 'Zeit'])
 result=[]
+
+for i in range(len(tf)) :
+	if tf.loc[i,"Name"] == "summemax":
+		summemaxtemp = float(tf.loc[i,"Value"])
+		summemaxtimetemp = str(tf.loc[i,"Zeit"])
+		if summemax < summemaxtemp:
+			summemax = summemaxtemp
+			summemaxtime = summemaxtimetemp
+	if tf.loc[i,"Name"] == "btcmax":
+		btcmaxtemp = float(tf.loc[i,"Value"])
+		btcmaxtimetemp = str(tf.loc[i,"Zeit"])
+		if btcmax < btcmaxtemp:
+			btcmax = btcmaxtemp
+			btcmaxtime = btcmaxtimetemp
 
 class CryptoTicker:
 	def __init__(self, master):
 		self.master = master
 		self.close_button = Button(text="C", command=self.close)
 		self.close_button.grid(row=0, column=0)
-		self.label = Label(master, text="DeFi Dashboard",anchor=W, justify=LEFT, font=('Helvetica',32, 'bold'), fg = 'blue')
-		self.label.grid(row=0, column=1, columnspan=1)
+
+		self.label = Label(master, text="Crypto Dashboard",justify=LEFT, width=20, font=('Helvetica',32, 'bold'), fg = 'blue')
+		self.label.grid(row=0, column=1, columnspan=2)
 
 	def labels():
 		hwg()
 
 #		investbtc = summepurchase * 1000 / purchasebtc
-		text1 = str(portsummemaxprint)
-		down_label = Label(text=(text2 + '\n' + text3a + '\n' + text3 + text5 + '\n' + text9 + '\n' + text6),anchor=NW, width = 19, height=3, justify=LEFT,font=('Helvetica',25))
-		down_label.grid(row=2, column=1)
-		
-        currency = "{:,.2f}".format(investbtc)
-		text1 = "str(btcmaxtime) #.strftime("%Y-%m-%d %H:%M")
+		currency = "{:,.2f}".format(investbtc)
+		text1 = str(btcmaxtime) #.strftime("%Y-%m-%d %H:%M")
 		text2 =  "Portfolio____: " + u'\u20bf' + str(btcprint)
 		text3a =  "Portfolio Start: " + u'\u20bf' + str(currency)
 		text3 =  "Portfolio ATH: " + u'\u20bf' + str(btcmaxprint)
+		down_label = Label(text=(text2 + '\n' + text3a + '\n' + text3 ),anchor=NW, width = 19, height=3, justify=LEFT,font=('Helvetica',25))
+		down_label.grid(row=2, column=1)
+		
 		currency = "${:,.2f}T".format(investusd)
 		text5 = "Portfolio____: " + str(summeprint)
 		text9 = "Portfolio Start: " + str(currency)
 		text6 = "Portfolio ATH: " + str(summemaxprint)
-		down_label = Label(text=(text2 + '\n' + text3a + '\n' + text3 + text5 + '\n' + text9 + '\n' + text6),anchor=NW, width = 19, height=3, justify=LEFT,font=('Helvetica',25))
-		down_label.grid(row=2, column=1)
-		
+		down_label = Label(text=(text5 + '\n' + text9 + '\n' + text6), anchor=NW, width = 19, height=3, justify=LEFT,font=('Helvetica',25))
+		down_label.grid(row=2, column=2)
+
 		text4 = "ATH $ date: " + str(summemaxtime)
-	
+		down_label = Label(text=(text4), anchor=NW, width = 39, height=2, justify=LEFT,font=('Helvetica',12))
+		down_label.grid(row=3, column=2)
+		
 		currency = "${:,.2f}".format(pricespec1)
 		text7 = str(specialcoin1) + ": " + str(currency)
 		currency = "${:,.2f}".format(pricespec2)
 		text8 = str(specialcoin2) + ": " + str(currency)
+		down_label = Label(text=(text7 + '\n' + text8), anchor=NW, width = 19, justify=LEFT,font=('Helvetica',25, 'bold'))
+		down_label.grid(row=4, column=1)
+
 		invperc = summe / investusd
 		currency = "{:,.0%}".format(invperc)
 		text10b = "RoI: " + str(currency)
 		text10 = "Index: " + str(fearindex) + " / " + str(fearindexvalue)
-		down_label = Label(text=(text7 + '\n' + text8 + text10 + '\n' + text10b), anchor=NW, width = 19, justify=LEFT,font=('Helvetica',25, 'bold'))
-		down_label.grid(row=4, column=1)
-
-#		down_label = Label(text=(), anchor=W, width = 19, bg='#111118', justify=LEFT,relief=RAISED, font=('Helvetica',25,'bold'), fg='white')
+		down_label = Label(text=(text10 + '\n' + text10b), anchor=W, width = 19, bg='#111118', justify=LEFT,relief=RAISED, font=('Helvetica',25,'bold'), fg='white')
+		down_label.grid(row=4, column=2)
 
 # Row 5 to 9 = bottom/top 4 performing coins (profit)
 #a		text98 = "Losers last 24hrs"
 		text98 = "Losers"
-		text99 = "Winners"
-		down_label = Label(text=(text98 + text99), anchor=SW, width = 19, height=1, justify=LEFT,font=('Helvetica',20))
+		down_label = Label(text=(text98), anchor=SW, width = 19, height=1, justify=LEFT,font=('Helvetica',20))
 		down_label.grid(row=5, column=1)
+#a		text99 = "Winners last 24hrs"
+		text99 = "Winners"
+		down_label = Label(text=(text99), anchor=SW, width = 19, height=1, justify=LEFT,font=('Helvetica',20))
+		down_label.grid(row=5, column=2)
+
+		text11=str(loose.Coin.iloc[0])
+		currencya = "{:,.0%}".format(loose.result.iloc[0])
+		text11a = str(currencya)
+		text12=str(loose.Coin.iloc[1])
+		currencyb = "{:,.0%}".format(loose.result.iloc[1])
+		text12a = str(currencyb)
+		text13=str(loose.Coin.iloc[2])
+		currencyc = "{:,.0%}".format(loose.result.iloc[2])
+		text13a = str(currencyc)
+		text14=str(loose.Coin.iloc[3])
+		currencyd = "{:,.0%}".format(loose.result.iloc[3])
+		text14a = str(currencyd)
+		down_label = Label(text=(text11 + ': ' + text11a + '\n' +  text12 + ': ' + text12a + '\n' + text13 + ': ' + text13a + '\n' +  text14 + ': ' + text14a), anchor=NW, width = 19, relief=RAISED, height=4, justify=LEFT,font=('Helvetica',20, 'bold'), fg='red')
+		down_label.grid(row=6, column=1)
+
+		text21=str(win.Coin.iloc[0])
+		currencya = "{:,.0%}".format(win.result.iloc[0])
+		text21a = str(currencya)
+		text22=str(win.Coin.iloc[1])
+		currencyb = "{:,.0%}".format(win.result.iloc[1])
+		text22a = str(currencyb)
+		text23=str(win.Coin.iloc[2])
+		currencyc = "{:,.0%}".format(win.result.iloc[2])
+		text23a = str(currencyc)
+		text24=str(win.Coin.iloc[3])
+		currencyd = "{:,.0%}".format(win.result.iloc[3])
+		text24a = str(currencyd)
+		down_label = Label(text=(text21 + ': ' + text21a + '\n' +  text22 + ': ' + text22a + '\n' + text23 + ': ' + text23a + '\n' +  text24 + ': ' + text24a), anchor=NW, width = 19, relief=RAISED, height=4, justify=LEFT,font=('Helvetica',20, 'bold'), fg='green')
+		down_label.grid(row=6, column=2)
+
 
 # This is where you set the update time. 1000 - 1 sec	
 		down_label.after(180000,CryptoTicker.labels)
@@ -126,10 +184,8 @@ def hwg():
 		for i in range(len(df)) :
 			qtycoin = float(df.loc[i,"Qty"])
 			purchasecoin =  float(df.loc[i,"Purchase"])
-			https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken
-
 			pricecoin = float(loads(urlopen('https://api.coingecko.com/api/v3/coins/' + df.loc[i,"Coin"]).read())['market_data']['current_price']['usd'])
-#a			sellcoinperc = float(loads(urlopen('https://api.coingecko.com/api/v3/coins/' + df.loc[i,"Coin"]).read())['market_data']['price_change_percentage_24h'])
+#			sellcoinperc = float(loads(urlopen('https://api.coingecko.com/api/v3/coins/' + df.loc[i,"Coin"]).read())['market_data']['price_change_percentage_24h'])
 
 # "id":"dash"
 # "symbol":"dash"
@@ -140,7 +196,6 @@ def hwg():
 # "market_cap_rank":31
 # "fully_diluted_valuation":null
 # "total_volume":330371413
-# “high_24h":83.68
 # "low_24h":77.36
 # "price_change_24h":-5.59646068
 # "price_change_percentage_24h":-6.71763
@@ -174,7 +229,6 @@ def hwg():
 	try:
 		pricespec1 = float(loads(urlopen('https://api.coingecko.com/api/v3/coins/' + specialcoin1).read())['market_data']['current_price']['usd'])
 		pricespec2 = float(loads(urlopen('https://api.coingecko.com/api/v3/coins/' + specialcoin2).read())['market_data']['current_price']['usd'])
-
 	except:
 		print("Error reading Coin URL", specialcoin1)
 
@@ -182,12 +236,8 @@ def hwg():
 #	get the fearindex
 		fearindex = str(loads(urlopen('https://api.alternative.me/fng/').read())['data'][0]['value_classification'])
 		fearindexvalue = str(loads(urlopen('https://api.alternative.me/fng/').read())['data'][0]['value'])
-
-		standardgasprice = float(loads(urlopen('https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken').read())['result']['SafeGasPrice'])
-		fastgasprice = float(loads(urlopen('https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=YourApiKeyToken').read())['result']['FastGasPrice'])
-
 	except:
-		print("Error reading Fearindex or gasprice URL")
+		print("Error reading Fearindex URL")
 
 
 #	collecting top gainers and losers
