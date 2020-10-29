7c199c10a53a49abc870094fa99357324a919ee4
rfkinline <robert@lightdec.com>
2020-10-29 10:58:52 -0400

    i got it working

--- test Bitquery.py
+++ test Bitquery.py
@@ -78,12 +78,13 @@ def run_query(query):  # A simple function to use request$
 query = """query($address:String!){ethereum{address(address: {is: $address}){balances{currency{symbol, name,address}value}}}}"""
 result = run_query(query)
 b=json.dumps(result)
-jsondata=json.loads(b)
-#jsondata='''{"data":{"ethereum":{"address":[{"balances":[{"currency":{"symbol":"UNI","name":"Uniswap","address":"0x1f9840a85d5af5bf1d1762f925bdaddc4201f984","decimals":18},"value":2.288247},{"currency":{"symbol":"UNI-V2","name":"Uniswap V2","address":"0xf49c43ae0faf37217bdcb00df478cf793edd6687","decimals":18},"value":26.291958},{"currency":{"symbol":"RSR","name":"Reserve Rights","address":"0x8762db106b2c2a0bccb3a80d1ed41273552616e8","decimals":18},"value":0},{"currency":{"symbol":"UNI-V2","name":"Uniswap V2","address":"0xc5be99a02c6857f9eac67bbce58df5572498f40c","decimals":18},"value":0},{"currency":{"symbol":"ZRX","name":"0x Protocol Token","address":"0xe41d2489571d322189246dafa5ebde1f4699f498","decimals":18},"value":0},{"currency":{"symbol":"STAKE","name":"STAKE","address":"0x0ae055097c6d159879521c384f1d2123d1f195e6","decimals":18},"value":0},{"currency":{"symbol":"ETH","name":"Ether","address":"-","decimals":18},"value":0.01293373},{"currency":{"symbol":"VIDT","name":"VIDT Datalink","address":"0xfef4185594457050cc9c23980d301908fe057bb1","decimals":18},"value":506.88068},{"currency":{"symbol":"KNC","name":"Kyber Network Crystal","address":"0xdd974d5c2e2928dea5f71b9825b8b646686bd200","decimals":18},"value":0},{"currency":{"symbol":"AMPL","name":"Ampleforth","address":"0xd46ba6d942050d489dbd938a2c909a5d5039a161","decimals":9},"value":76.24007}]}]}}}'''
-jsondata = jsondata[32:]
+a=json.loads(b)
+jsondata=json.dumps(a)
+jsondata = jsondata[35:]
 jsondata = jsondata[:-4]
-
+#print("AAAAA",jsondata)
 df=pd.read_json(jsondata,orient='columns')
+#jsondata='''{"data":{"ethereum":{"address":[{"balances":[{"currency":{"symbol":"UNI","name":"Uniswap","address":"0x1f9840a85d5af5bf1d1762f925bdaddc4201f984","decimals":18},"value":2.288247},{"currency":{"symbol":"UNI-V2","name":"Uniswap V2","address":"0xf49c43ae0faf37217bdcb00df478cf793edd6687","decimals":18},"value":26.291958},{"currency":{"symbol":"RSR","name":"Reserve Rights","address":"0x8762db106b2c2a0bccb3a80d1ed41273552616e8","decimals":18},"value":0},{"currency":{"symbol":"UNI-V2","name":"Uniswap V2","address":"0xc5be99a02c6857f9eac67bbce58df5572498f40c","decimals":18},"value":0},{"currency":{"symbol":"ZRX","name":"0x Protocol Token","address":"0xe41d2489571d322189246dafa5ebde1f4699f498","decimals":18},"value":0},{"currency":{"symbol":"STAKE","name":"STAKE","address":"0x0ae055097c6d159879521c384f1d2123d1f195e6","decimals":18},"value":0},{"currency":{"symbol":"ETH","name":"Ether","address":"-","decimals":18},"value":0.01293373},{"currency":{"symbol":"VIDT","name":"VIDT Datalink","address":"0xfef4185594457050cc9c23980d301908fe057bb1","decimals":18},"value":506.88068},{"currency":{"symbol":"KNC","name":"Kyber Network Crystal","address":"0xdd974d5c2e2928dea5f71b9825b8b646686bd200","decimals":18},"value":0},{"currency":{"symbol":"AMPL","name":"Ampleforth","address":"0xd46ba6d942050d489dbd938a2c909a5d5039a161","decimals":9},"value":76.24007}]}]}}}'''
 rows = []
 for i,r in df.iterrows():
 	rows.append({'eventid':i+1,'symbol':r['balances']['currency']['symbol'], 'name': r['balances']['currency']['name'],'value':r['balances']['value']})
